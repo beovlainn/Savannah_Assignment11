de161da552f785e4d0f980649880d0a942e6d0de
Ian McDaniel <tyler.ian.mcdaniel@hotmail.com>
2025-04-16 23:52:18 -0400

    Add project files.

---
+++ Savannah_Assignment11/mainPackage/main.py
@@ -0,0 +1,10 @@
+#File Name : main.py
+#Student Name: Ian McDaniel, Evan Bolin
+#email:  mcdaniip@mail.uc.edu, bolinen@mail.uc.edu
+#Assignment Number: Assignment 11
+#Due Date:   April 17 2025
+#Course #/Section:   IS4010-001
+#Semester/Year:   Spring 2025
+#Brief Description of the assignment:   Using api and python to clean up a csv file and create two new ones 
+#Brief Description of what this module does.: Furthering our knowledge of the python language with api usage and csv experience 
+#Citations: https://chatgpt.com
